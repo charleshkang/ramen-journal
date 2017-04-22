d41964637994127b97916d4bd8d4084af044f159
charleshkang <charleshkang1@gmail.com>
2017-04-22 00:47:33 -0400

    added pods, initial project setup

---
+++ ramen-map/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  ramen-map
+//
+//  Created by Charles Kang on 4/21/17.
+//  Copyright © 2017 Charles Kang. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Ramen Map"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
